e4001e97e40dedc32cd55795652945dd4c72e974
Markus Frosch <markus.frosch@icinga.com>
2018-02-06 12:55:09 +0100

    testing: Respect docroot on SUSE

--- testing/start_test.sh
+++ testing/start_test.sh
@@ -5,4 +5,9 @@ install_package icinga2-templates
 
 ls -al /usr/share/icinga2/include
 cat /usr/share/icinga2/include/itl
-ls -al /usr/share/doc/icinga2-templates*/
+if [ -e /usr/share/doc/packages/icinga2-templates ]; then
+  # docroot on SUSE
+  ls -al /usr/share/doc/packages/icinga2-templates
+else
+  ls -al /usr/share/doc/icinga2-templates*/
+fi
